0d3f1902c7890e6abfea78e7ebeeff1c95c09694
f1ne2 <73852659+f1ne2@users.noreply.github.com>
2020-11-04 17:51:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,25 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'{name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Hello word')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+choice = "y"
+i = 0
+name2 = []
+while choice.lower() == "y":
+    name = str(input("Введите элемент массива"))
+    name2.append(name)
+    i += 1
+    choice = str(input("Для ввода следующего элемента нажмите y, а для выхода любую другую клавишу"))
+    if choice == "y":
+        continue
+    else:
+        break
+new = []
+n = 0
+k=i
+if i>0:
+    while n < i:
+        new.append(name2[k-1])
+        n += 1
+        k -= 1
+print(name2)
+print(new)
