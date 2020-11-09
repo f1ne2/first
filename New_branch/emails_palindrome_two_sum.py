60ead7b37936644dc4b972bd9a3ce648f32f8c8f
f1ne2 <73852659+f1ne2@users.noreply.github.com>
2020-11-09 23:48:23 +0300

    Update emails_palindrome_two_sum.py

--- New_branch/emails_palindrome_two_sum.py
+++ New_branch/emails_palindrome_two_sum.py
@@ -8,13 +8,12 @@ class Solution:
                 local = local[:local.index("+")]
             local = local.replace(".", "")
             emails_edit.append(local + "@" + domain)
-        result = len(set(emails_edit))
-        return result
+        return len(set(emails_edit))
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         reverse_string = str(x)[::-1]
-        return(str(x) == reverse_string)
+        return str(x) == reverse_string
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
