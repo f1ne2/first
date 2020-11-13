4b5573ef11679ad83f196ab0acd912e958f9eb98
python1 <alextomilovski@gmail.com>
2020-11-14 02:33:50 +0300

    14.11.20

--- matrixs/Game.py
+++ matrixs/Game.py
@@ -2,69 +2,59 @@
 import os
 # import sleep to show output for some time period
 from time import sleep
-
 from random import randint
 
-# создание исходной матрицы по заданным параметрам
-def original_matrix(a,b,dot):
-    x = set()
-    matrix1 = [[0] * b for i in range(a)]
-    for t in range(dot):
-        i,j = randint(0,a-1),randint(0,b-1)
-        string = str(i) + str(j)
-        print(string)
-        if string not in x:
-            x.add(string)
+# creation initial matrix
+def original_matrix(w,h,dot):
+    x = 0
+    matrix1 = [[0] * h for i in range(w)]
+    while x < dot:
+        i,j = randint(0,w-1),randint(0,h-1)
+        if matrix1[i][j] == 0:
             matrix1[i][j] = 1
-        else:
-            t -= 1
-            print(t)
-    print(x)
+            x += 1
     return matrix1
 
-# Вычисление новой матрицы
-def result_matrix(a,b,matrix):
-    result = []
-    matrix_with_zero = [[0] * (b+2) for k in range(a+2)]
-    for x in range(a+2):
-        for y in range(b+2):
-            if x == 0 or y == 0 or x == a+1 or y == b+1:
+# computation of new matrix
+def result_matrix(matrix):
+    matrix_with_zero = [[0] * (len(matrix[0])+2) for k in range(len(matrix)+2)]
+    for x in range(len(matrix)+2):
+        for y in range(len(matrix[0])+2):
+            if x == 0 or y == 0 or x == len(matrix)+1 or y == len(matrix[0])+1:
                 matrix_with_zero[x][y] = 0
             else:
-                matrix_with_zero[x][y] = matrix[x - 1][y - 1]
-    zero_matrix = [[0] * (b+2) for k in range(a+2)]
-    for i in range(1, len(zero_matrix)-1):
-        for j in range(1, len(zero_matrix[i])-1):
+                matrix_with_zero[x][y] = matrix[x-1][y-1]
+    new_matrix = [[0] * (len(matrix[0])+2) for k in range(len(matrix)+2)]
+    for i in range(1, len(new_matrix)-1):
+        for j in range(1, len(new_matrix[i])-1):
             sum = matrix_with_zero[i-1][j] + matrix_with_zero[i+1][j] + matrix_with_zero[i][j+1] + matrix_with_zero[i][j-1]\
-            + matrix_with_zero[i-1][j-1] + matrix_with_zero[i+1][j+1] + matrix_with_zero[i-1][j+1] + matrix_with_zero[i+1][j-1]
-            if matrix_with_zero[i][j] == 0:
-                if sum != 3:
-                    zero_matrix[i][j] = 0
-                else:
-                    zero_matrix[i][j] = 1
+                + matrix_with_zero[i-1][j-1] + matrix_with_zero[i+1][j+1] + matrix_with_zero[i-1][j+1] + matrix_with_zero[i+1][j-1]
+            if matrix_with_zero[i][j] == 0 and sum != 3:
+                new_matrix[i][j] = 0
+            else:
+                new_matrix[i][j] = 1
             if matrix_with_zero[i][j] == 1:
                 if sum < 2 or sum > 3:
-                    zero_matrix[i][j] = 0
+                    new_matrix[i][j] = 0
                 else:
-                    zero_matrix[i][j] = 1
-    for k in range(1,len(zero_matrix)-1):
-        del zero_matrix[k][0]
-        del zero_matrix[k][len(zero_matrix[k])-1]
-    result = zero_matrix[1:len(zero_matrix)-1]
+                    new_matrix[i][j] = 1
+    result = [[0] * (len(new_matrix[0]) - 2) for k in range(len(new_matrix) - 2)]
+    for k in range(1,len(new_matrix)-1):
+        for q in range(1,len(new_matrix[0])-1):
+            result[k-1][q-1] = new_matrix[k][q]
     return result
 
-# исходные данные по размерам поля и числу шагов
-r = int(input("Введите число строк поля"))
-c = int(input("Введите число столбцов поля"))
-alive_dot = int(input("Введите число живых точек"))
-# исходный массив игры
+# initial data
+r = int(input("Rows of the matrix"))
+c = int(input("Columns of the matrix"))
+alive_dot = int(input("Alive dot"))
+# initial array
 m1 = original_matrix(r,c,alive_dot)
 print(m1)
-# выходные массивы игры
-g = 0
-while g == 0:
+# outputting arrays
+while True:
     sleep(2)
     os.system("cls")
-    m1 = result_matrix(r,c,m1)
+    m1 = result_matrix(m1)
     print(m1)
 
